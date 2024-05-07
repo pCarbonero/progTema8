741574ba17c50abc3a5befd44f2405fca85c0bf4
pCarbonero <pablo.carbonero@iesnervion.es>
2024-05-07 14:37:09 +0200

    [new] tema 8 01 mitad

---
+++ Tema8/src/ejercicio01/Main.java
@@ -0,0 +1,45 @@
+package ejercicio01;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int numInt = -10000;
+		double numDb = -10000;
+		int opc;
+		
+		do {
+			menu();
+			opc = sc.nextInt();
+			sc.nextLine();
+			
+			switch (opc) {
+			case 1: {
+				numInt = Utils.readInt(sc);
+				break;
+			}
+			case 2: {
+				numDb = Utils.readInt(sc);
+				break;
+			}
+			case 3: {
+				System.out.println("Has salido ");
+				break;
+			}
+			default:
+				System.out.println("Opción no válida ");
+			}
+			
+		
+		} while (opc != 3);
+
+	}
+	
+	static void menu() {
+		System.out.println("1. Integer");
+		System.out.println("2. Double");
+		System.out.println("3. Salir");
+	}
+}
---
+++ Tema8/src/ejercicio01/Utils.java
@@ -0,0 +1,33 @@
+package ejercicio01;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Utils {
+	
+	public static int readInt(Scanner sc) {
+		int num;
+		try {
+			System.out.println("Inserta un numero entero ");
+			num = sc.nextInt();
+			System.out.println("El número es " + num); 
+		}catch(InputMismatchException e){
+			num = 0;
+			System.out.println("El valor insertado no es de tipo int ");
+		}	
+		return num;
+	}
+	
+	public static double readDouble(Scanner sc) {
+		double num;
+		try {
+			System.out.println("Inserta un numero decimal ");
+			num = sc.nextDouble();
+			System.out.println("El número es " + num); 
+		}catch(InputMismatchException e){
+			num = 0;
+			System.out.println("El valor insertado no es de tipo double ");
+		}	
+		return num;
+	}
+}
